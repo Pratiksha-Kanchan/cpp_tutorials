6dae56aa22a6cae602c139df0d19be25a06f1131
Pratiksha Kanchan <84810132+Unmerited-Scientist278@users.noreply.github.com>
2022-06-07 17:06:20 +0530

    Create string_word.cpp

---
+++ array/string_word.cpp
@@ -0,0 +1,35 @@
+// Example 1: C++ String to read a word
+
+// C++ program to display a string entered by user.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[100];
+
+    cout << "Enter a string: ";
+    cin >> str;
+    cout << "You entered: " << str << endl;
+
+    cout << "\nEnter another string: ";
+    cin >> str;
+    cout << "You entered: "<<str<<endl;
+
+    return 0;
+}
+
+// Output
+
+// Enter a string: C++
+// You entered: C++
+
+// Enter another string: Programming is fun.
+// You entered: Programming
+
+
+/*
+Notice that, in the second example only "Programming" is displayed instead of "Programming is fun".
+
+This is because the extraction operator >> works as scanf() in C and considers a space " " has a terminating character. */
