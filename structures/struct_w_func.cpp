ce4e66abd88e7187db99ae02c309a85c8e4160dd
Pratiksha Kanchan <84810132+Unmerited-Scientist278@users.noreply.github.com>
2022-06-07 17:38:46 +0530

    Create struct_w_func.cpp

---
+++ structures/struct_w_func.cpp
@@ -0,0 +1,56 @@
+// Example 1: C++ Structure and Function
+
+#include <iostream>
+using namespace std;
+
+struct Person {
+    char name[50];
+    int age;
+    float salary;
+};
+
+void displayData(Person);   // Function declaration
+
+int main() {
+    Person p;
+
+    cout << "Enter Full name: ";
+    cin.get(p.name, 50);
+    cout << "Enter age: ";
+    cin >> p.age;
+    cout << "Enter salary: ";
+    cin >> p.salary;
+
+    // Function call with structure variable as an argument
+    displayData(p);
+
+    return 0;
+}
+
+void displayData(Person p) {
+    cout << "\nDisplaying Information." << endl;
+    cout << "Name: " << p.name << endl;
+    cout <<"Age: " << p.age << endl;
+    cout << "Salary: " << p.salary;
+}
+
+// Output
+
+// Enter Full name: Bill Jobs
+// Enter age: 55
+// Enter salary: 34233.4
+
+// Displaying Information.
+// Name: Bill Jobs
+// Age: 55
+// Salary: 34233.4
+
+// In this program, user is asked to enter the name, age and salary of a Person inside main() function.
+
+// Then, the structure variable p is to passed to a function using.
+
+// displayData(p);
+
+// The return type of displayData() is void and a single argument of type structure Person is passed.
+
+// Then the members of structure p is displayed from this function.
