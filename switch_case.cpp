e5e76fa3c14e36a5683da6df34ba098701363520
Pratiksha Kanchan <84810132+Unmerited-Scientist278@users.noreply.github.com>
2022-06-07 11:33:36 +0530

    Create switch_case.cpp

---
+++ switch_case.cpp
@@ -0,0 +1,41 @@
+/* The expression is evaluated once and compared with the values of each case label.
+
+    If there is a match, the corresponding code after the matching label is executed. 
+    For example, if the value of the variable is equal to constant2, 
+    the code after case constant2: is executed until the break statement is encountered.
+    If there is no match, the code after default: is executed.
+ */
+
+// Program to build a simple calculator using switch Statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    char oper;
+    float num1, num2;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> oper;
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2;
+
+    switch (oper) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+        default:
+            // operator is doesn't match any case constant (+, -, *, /)
+            cout << "Error! The operator is not correct";
+            break;
+    }
+
+    return 0;
+}
