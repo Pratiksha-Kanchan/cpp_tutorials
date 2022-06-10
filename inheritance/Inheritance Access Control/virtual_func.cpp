3951f2314b5835b7e47e5650b0f7b6782a2679ae
Pratiksha Kanchan <84810132+Unmerited-Scientist278@users.noreply.github.com>
2022-06-10 11:25:34 +0530

    Create virtual_func.cpp

---
+++ inheritance/Inheritance Access Control/virtual_func.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+   public:
+    virtual void print() {
+        cout << "Base Function" << endl;
+    }
+};
+
+class Derived : public Base {
+   public:
+    void print() {
+        cout << "Derived Function" << endl;
+    }
+};
+
+int main() {
+    Derived derived1;
+
+    // pointer of Base type that points to derived1
+    Base* base1 = &derived1;
+
+    // calls member function of Derived class
+    base1->print();
+
+    return 0;
+}
+
+// Output
+
+// Derived Function
+
+/*
+Here, we have declared the print() function of Base as virtual.
+
+So, this function is overridden even when we use a pointer of Base type that points to the Derived object derived1.
+*/
